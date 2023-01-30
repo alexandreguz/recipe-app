4cf2dfa655f749f9ff32941d56fd14418130147a
Alexandre <alexandre@weel.com>
2023-01-30 15:08:37 +0200

    recipe-component receiving data from recipe-item through recipe-list

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,46 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('A test recipe', 'this is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
-  ];
+    recipes: Recipe[] = [
+            new Recipe('A test recipe', 'this is a simple test', 'https://upload.wikimedia.org/wikipedia/commons/5/57/990402-ians-recipe-01-IMG_4724.jpg')
+           ];
+      @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
     
     constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
 }
+
+// import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+// import { Recipe } from '../recipe.model';
+
+// @Component({
+//   selector: 'app-recipe-list',
+//   templateUrl: './recipe-list.component.html',
+//   styleUrls: ['./recipe-list.component.css']
+// })
+// export class RecipeListComponent implements OnInit {
+//   recipes: Recipe[] = [
+//     @Output() recipeWasSelected = new EventEmitter<Recipe>();
+//     new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+//     new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+//   ];
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+//   onRecipeSelected(recipe: Recipe) {
+//    this.recipeWasSelected.emit(recipe);
+//   }
+
+// }
